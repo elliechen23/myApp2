b5dd7140357bc7f14c24b00b35e72c15db9b51bc
Ellie Chen <elliechen@example.com>
2018-02-11 22:29:22 +0800

    2018-02-11 create
    RestSharp console sample

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace myApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //var client = new RestClient("http://example.com");
+            //client.Authenticator = new SimpleAuthenticator("username", "foo", "password", "bar");
+            //var request = new RestRequest("resource", Method.GET);
+            //var queryResult = client.Execute(request);
+            //Console.WriteLine(queryResult.Content);
+
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("posts/1", Method.GET); 
+                    
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var queryResult = client.Execute(request);
+            Console.WriteLine(queryResult.Content);
+
+            
+
+            
+
+        }
+            
+    }
+}
